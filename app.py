f7c940d665fdbee132fe1abff94e1fce625b1b83
tsutskovmaksim-stack <tsutskov.maksim@bk.ru>
2026-07-27 16:08:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -177,8 +177,7 @@ if choice == "🤖 Чат с Lottery":
             # Логика ответов
             user_text_lower = user_input.lower()
             if "создател" in user_text_lower or "создал" in user_text_lower or "разработчик" in user_text_lower or "автор" in user_text_lower:
-                ai_response = "Моим создателем и единственным разработчиком является великий гений Loter! Гордись, что он разрешил тебе писать мне!"
-          elif "имя" in user_text_lower or "зовут" in user_text_lower:
+                ai_response = "Моим создателем и единственным разработчиком является великий гений Loter! Гордись, что он разрешил тебе писать мне!elif "имя" in user_text_lower or "зовут" in user_text_lower:
                 ai_response = "Меня зовут Lottery!🎰 И не смей забывать мое имя!"
             else:
                 ai_response = random.choice(jokes)
